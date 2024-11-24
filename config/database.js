8c99f818e077c850ffbe112d452cfc73346f52ad
Yash Tiwari <yashutiwari22@gmail.com>
2024-11-24 07:14:44 +0530

    Delete config directory

--- config/database.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-const dbConnect = async()=>{
-    await mongoose.connect("mongodb+srv://yash:tiwari@cluster0.uj9zl7s.mongodb.net/devHub" );
-}
-
-module.exports = dbConnect ; 
